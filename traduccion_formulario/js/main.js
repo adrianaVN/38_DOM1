e1fb75cd99042933c29e6b2a19e6974fd4772153
adrianaVN <venegasadriana78@gmail.com>
2016-10-17 16:33:25 +0000

    traduccion formulario completo

--- traduccion_formulario/js/main.js
+++ traduccion_formulario/js/main.js
@@ -2,13 +2,18 @@ function cambiarIdioma(idioma){
 var sing=document.getElementById('singIn');
 var mail= document.getElementById('inputEmail');
 var password= document.getElementById('inputPassword');
-var recuerda= document.getElementById('checkBox');
 if (idioma =='es') {
+	document.getElementById("form-signin-heading").innerHTML = "Entra por favor";
 	mail.setAttribute('placeholder','ingresar correo');
 	password.setAttribute('placeholder','ingresar contraseña');
+	document.getElementById("check").innerHTML ="Recordar datos";
+	document.getElementById("boton").innerHTML ="Entrar";
 }else if (idioma == 'en') {
+	document.getElementById("form-signin-heading").innerHTML = "Please sign in";
 	mail.setAttribute('placeholder','Please enter your email');
 	password.setAttribute('placeholder','password');
+	document.getElementById("check").innerHTML ="Remember me";
+	document.getElementById("boton").innerHTML ="Sign in";
 }
 }
 
