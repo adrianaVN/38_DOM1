508a72ad18ecc4e49957fea673da04fbf0a9b372
adrianaVN <venegasadriana78@gmail.com>
2016-10-17 21:58:41 +0000

    bicis locas terminado

--- bicis_locas/js/main.js
+++ bicis_locas/js/main.js
@@ -1,3 +1,31 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	var nombre, apellido, mail, contraseña;
+	nombre= document.getElementById('name').value;
+	apellido= document.getElementById('lastname').value;
+	mail = document.getElementById('input-email').value;
+	contraseña= document.getElementById('input-password').value;
+	if (nombre=="" || apellido=="" || mail=="" || contraseña=="") {
+		alert('todos los campos son obligatorios');
+		return false 
+	}
+	else if (contraseña.length <6){
+		alert('la contraseña es muy corta');
+		return false;
+	}
+	else if (contraseña=='contraseña' || contraseña==123456 || contraseña==098754) {
+		alert('esta contraseña no esta permitida');
+		return false;
+	}
+	else if(/[a-z]/.test(nombre[0]) || /[a-z]/.test(apellido[0])){
+		alert('unicamente la primera letra debe ser mayuscula');
+		return false;
+	}
+	else if (!/^[a-zA-Z]*$/g.test(document.getElementById('lastname').value)) {
+		alert('el apellido debe contener solo letras');
+		return false;
+	}
+	else if (!/^[a-zA-Z]*$/g.test(document.getElementById('name').value)) {
+		alert('el nombre debe contener solo letras');
+		return false;
+	}
 }
